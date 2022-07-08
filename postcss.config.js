1b7a85dc4cbf16e0c6665e734532e5c6d0e7e488
Frank Zheng <zmyfrank@qq.com>
2022-07-08 10:01:46 +0800

    fix: 删除了不需要的tailwindcss配置,解决了样式冲突问题

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
+    // 'tailwindcss/nesting': 'postcss-nesting',
+    // tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
